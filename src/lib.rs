2e82e85e73071cae22ad7725b3a4a446dae807fb
Taha MM <fadilrezokt@gmail.com>
2026-06-11 11:15:03 +0100

    Refactor imports for heston and bates modules

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ pub use types::{OptionType, OptionContract, PricingResult, IvProblem,
                 HestonParams, BatesParams, LocalVolSurface};
 pub use bsm::{bsm_price, bsm_price_and_greeks, black76_price_and_greeks};
 pub use iv::implied_vol;
-pub use heston::heston_price;
-pub use bates::bates_price;
+pub use heston::{heston_price, heston_price_and_greeks};
+pub use bates::{bates_price, bates_price_and_greeks};
 pub use local_vol::{dupire_local_vol, monotone_cubic_interp};
 pub use batch::{batch_bsm, batch_bsm_price, batch_heston, batch_bates, batch_implied_vol};
